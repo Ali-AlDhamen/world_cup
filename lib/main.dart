d3ab331d07ddadb53e2d0b11a1d574642d39d5fa
Pali2002 <ali77dhamen@hotmail.com>
2022-11-27 19:35:37 +0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:world_cup/screens/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+// ...
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -16,7 +24,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       routes: {
-       WelcomePage.routeName: (context) => const WelcomePage(),
+        WelcomePage.routeName: (context) => const WelcomePage(),
       },
     );
   }
