d7e8bc53517318af6b138a9150f58249133e3301
Pali2002 <ali77dhamen@hotmail.com>
2022-11-27 19:17:21 +0300

    welcome page

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,7 +4,8 @@ const Color kWhiteColor = Color(0xFFFFFFFF);
 const Color kBlackColor = Color(0xFF1E1E1E);
 const Color kOrangeColor = Color(0xFFFF4700);
 const Color kBurgundyColor = Color(0xFF8A1538);
-const Color kGrayColor = Color.fromRGBO(255, 255, 255, 0.5);
+const Color kGrayColor = Color(0xFFC8C8C8);
+const Color kWhite20Color = Color.fromRGBO(255, 255, 255, 0.5);
 const Color kPinkColor = Color(0xFFFF004C);
 
 
