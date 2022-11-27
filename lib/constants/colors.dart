c6a9fbe21e23819d463eb73e3a425aa875c779c8
Pali2002 <ali77dhamen@hotmail.com>
2022-11-27 16:06:47 +0300

    colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kBlackColor = Color(0xFF1E1E1E);
+const Color kOrangeColor = Color(0xFFFF4700);
+const Color kBurgundyColor = Color(0xFF8A1538);
+const Color kGrayColor = Color.fromRGBO(255, 255, 255, 0.5);
+const Color kPinkColor = Color(0xFFFF004C);
+
+
+
+
