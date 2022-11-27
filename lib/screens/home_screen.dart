054061fc907259e993ac1c291f51c33a99e7b2a0
Pali2002 <ali77dhamen@hotmail.com>
2022-11-28 00:46:13 +0300

    nav screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,32 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:world_cup/screens/news_screen.dart';
+import 'package:world_cup/screens/profile_screen.dart';
+import 'package:world_cup/screens/results_screen.dart';
 import '../constants/colors.dart';
 import '../logins/login_controller.dart';
+import 'main_screen.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   static const String routeName = "/home";
   const HomeScreen({super.key});
-
   @override
-  Widget build(BuildContext context , WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  List<Widget> pages = [ProfileScreen() , ResultScreen() , NewsScreen(), MainScreen()];
+  int pageIndex =0;
+ void selectPage(int index){
+  setState(() {
+    pageIndex = index;
+  });
+ 
+ }
+  
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kBurgundyColor,
-        title: const Text("Home"),
+      backgroundColor: kBurgundyColor,
+      body: pages[pageIndex] ,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: selectPage,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: kWhite20Color,
+        unselectedItemColor: kWhiteColor,
+        selectedItemColor: kBurgundyColor,
+        currentIndex: pageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard_outlined),
+            label: 'Results',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_outlined),
+            label: 'News',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+        ],
       ),
-      body:  Center(
-        child: ElevatedButton(
-          onPressed: () {
-            ref.read(loginControllerProvider.notifier).signOut();
-          },
-           
-     
-          child: Text("sign_out"),
-        ),
-        )
-      );
-    
-     
-    
+
+    );
   }
 }
